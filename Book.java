f278b155e12f3b7bf7c27d3f338bcf2cfecb0ebd
Kai Yan <37325440+boyhuosr@users.noreply.github.com>
2019-03-25 17:17:50 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -49,17 +49,53 @@ public class Book {
     public int compareTo(Book other) {
 
         // YOUR CODE HERE
-        int result;
+        int result=0;
+        char[] thisAuthor = this.author.toCharArray();
+        char[] otherAuthor = other.author.toCharArray();
+        char[] thisTitle = this.title.toCharArray();
+        char[] otherTitle = other.author.toCharArray();
+
         if(this.author.equals(other.author) 
             && this.title.equals(other.title)
             && this.year==other.year){
             result = 0;
+        //If their authors are different
+        }else if(!this.author.equals(other.author)){
+            for(int i=0; i<this.author.length(); i++){
+                if(thisAuthor[i]<otherAuthor[i]){
+                    result = -1;
+                    break;
+                }else if(thisAuthor[i]>otherAuthor[i]){
+                    result = 1;
+                    break;
+                }else{i++;}
+
+            }
+        //if their authors are the same but with different titles
         }else if(this.author.equals(other.author) 
-            || this.title.equals(other.title)
-            || this.year==other.year){
-            result = -1;
-        }else{
-            result = 1;}
+            && !this.title.equals(other.title)){
+            for(int i=0; i<this.title.length(); i++){
+                if(thisTitle[i]<otherTitle[i]){
+                    result = -1;
+                    break;
+                }else if(thisTitle[i]>otherTitle[i]){
+                    result = 1;
+                    break;
+                }else{i++;}
+            }
+            if(this.title.length()<other.title.length()){
+                result = -1;
+            }
+        //if only their years are varied.
+        }else if(this.author.equals(other.author) 
+            && this.title.equals(other.title)
+            && this.year!=other.year){
+            if(this.year>other.year){
+                result=1;
+            }else{result = -1;}
+        }
+
         return result;    
     }
+
 }
