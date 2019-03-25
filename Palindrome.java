55e78046c004948471cda0a6d799a12b357c21be
Kai Yan <37325440+boyhuosr@users.noreply.github.com>
2019-03-25 10:31:58 -0400

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,28 +1,14 @@
-import java.util.Arrays;
 public class Palindrome{
 
 
 	private static boolean isPalindrome( String string ) {
 
 		// YOUR CODE HERE
-		String[] stringArr=string.split("\\.", 0);
-		int t = stringArr.length;
-		boolean result[];
-		result = new boolean[t];
-		String stringArr2[];
-		stringArr2 = stringArr;
-		int result2[];
-		result2 = new int[t];
 		
-		for(int i=0;i<stringArr.length;i++){
-			stringArr2[i]=StringUtils.withoutSpecials(stringArr[i]).toUpperCase();
+			string=StringUtils.withoutSpecials(string).toUpperCase();
 		
-			result[i] = stringArr2[i].equals(StringUtils.reverse(stringArr2[i]));
-			if(result[i]){result2[i]=1;}
-			else{result2[i]=0;}
-		}
-		//turn to String
-		return Arrays.toString(result2);
+			return string.equals(StringUtils.reverse(string));
+		
 	}
 
 	public static void palindromes( String string ) {
@@ -31,12 +17,12 @@ public class Palindrome{
 
 		// YOUR CODE HERE
 		String[] stringArr=string.split("\\.", 0);
-		byte [] newarry = isPalindrome(string).getBytes();
+		
 		for(int i=0; i<stringArr.length; i++){
-			if(newarry[i]==1){
-				System.out.println(stringArr[i]+ "is a Palindrome.");}
+			if(isPalindrome(stringArr[i])){
+				System.out.println(stringArr[i]+ " is a Palindrome.");}
 			else {
-				System.out.println(stringArr[i]+ "is not a Palindrome.");}
+				System.out.println(stringArr[i]+ " is not a Palindrome.");}
 		}
 		
 	}
